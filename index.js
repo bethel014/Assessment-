78b26be71ff5db042b5148f82a9cd47fa60b1044
bethel014 <144551342+bethel014@users.noreply.github.com>
2024-03-23 22:02:34 +0100

    create a function to handle the result
    by console logging the answer to the console when called

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function addNumber(num1, num2, num3, callback) {
   let sum = num1 + num2 + num3;
   callback(sum);
 }
+// define a fuction to handle the total sum
+function handleResult(sum) {
+  console.log("The total is :", sum);
+}
